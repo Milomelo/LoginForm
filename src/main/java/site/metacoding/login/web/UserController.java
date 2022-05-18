4638614c6a7aa15600fd35cd7ad02df4bbd64588
milo <mybule8@naver.com>
2022-05-18 15:10:05 +0900

    index 테스트 완료

---
+++ src/main/java/site/metacoding/login/web/UserController.java
@@ -0,0 +1,17 @@
+package site.metacoding.login.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public String main() {
+        return "index";
+    }
+
+}
