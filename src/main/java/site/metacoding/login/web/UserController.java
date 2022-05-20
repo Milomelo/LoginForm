06351e08d05d4946de101daca97d789e283e0322
milo <mybule8@naver.com>
2022-05-20 23:25:40 +0900

    주소 api테스트 완료

--- src/main/java/site/metacoding/login/web/UserController.java
+++ src/main/java/site/metacoding/login/web/UserController.java
@@ -3,6 +3,7 @@ package site.metacoding.login.web;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,12 +24,36 @@ public class UserController {
         return "joinForm";
     }
 
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
     @PostMapping("/join")
     public String join(@Valid JoinReqDto joinReqDto, BindingResult bindingResult) {
 
         UtilValid.요청에러처리(bindingResult);
         userService.회원가입(joinReqDto.toEntity());
         return "redirect:/joinForm";
+    }
+
+    @GetMapping("/popup/jusoPopup")
+    public String jusoPopup() {
+        return "/jusoPopup";
+    }
+
+    @GetMapping("/")
+    public String jusoApi() {
+        return "/sample";
+    }
+
+    @PostMapping("/popup/jusoPopup")
+    public String postMethodName(String inputYn, String roadFullAddr, Model model) {
+        System.out.println(inputYn);
+        System.out.println(roadFullAddr);
+        System.out.println(model.addAttribute("roadFullAddr", roadFullAddr));
+        System.out.println(model.addAttribute("inputYn", inputYn));
+        return "/jusoPopup";
     }
 
 }
